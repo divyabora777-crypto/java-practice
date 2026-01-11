badcf7ccec9f0d23aa8adeac1e0ce4ede7ac54dc
divyabora777-crypto <divyabora777@gmail.com>
2026-01-11 13:59:47 +0530

    Added maximum element program

---
+++ FindMaxElement.java
@@ -0,0 +1,14 @@
+public class FindMaxElement {
+    public static void main(String[] args) {
+        int[] arr = {12, 45, 7, 32};
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum element: " + max);
+    }
+}
